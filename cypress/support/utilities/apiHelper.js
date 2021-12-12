38522ebe884d67df3abb061e7a240af0111c403f
elraffy <37414789+elraffy@users.noreply.github.com>
2021-12-12 03:20:04 -0400

    readme final changes

--- cypress/support/utilities/apiHelper.js
+++ cypress/support/utilities/apiHelper.js
@@ -8,7 +8,7 @@ function getApi(){
     apiData.timestamp = date.getTime();
     apiData.publicKey = "5b76b5e9de1795f9b7fbd5dbe201ce51";
     apiData.privateKey = "5d121fd4cfbfd781f35fc0f0ba80f71a21e1d563";
-    apiData.hash = md5(apiData.timestamp + apiData.privateKey + apiData.publicKey );
+    apiData.hash = md5(apiData.timestamp + apiData.privateKey + apiData.publicKey);
     apiData.url = "https://gateway.marvel.com:443";
     apiData.limit = 100;    
     apiData.comicSerie = "X-Man";
