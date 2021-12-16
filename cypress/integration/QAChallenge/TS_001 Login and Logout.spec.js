9ed037e3fc2525e95a369cc1a5e438509aa1769c
elraffy <37414789+elraffy@users.noreply.github.com>
2021-12-16 17:52:38 -0400

    changes added

--- cypress/integration/QAChallenge/TS_001 Login and Logout.spec.js
+++ cypress/integration/QAChallenge/TS_001 Login and Logout.spec.js
@@ -4,12 +4,12 @@ const signIn = new SignInPage();
 
 describe('Login and Logout', function(){
 
-       it('Login success', function(){         
+       it('Login successfully', function(){         
         cy.login();
         cy.url().should('eq', Cypress.env('accountPageUrl'));       
     });
 
-    it('Logout success', function(){
+    it('Logout successfully', function(){
         cy.signOut();
         signIn.getAuthObject().should('be.visible');
     });
